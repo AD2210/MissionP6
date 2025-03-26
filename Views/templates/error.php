41ecd03ff9663e8b35893097430e5a103b4f7551
Anthony Delbée <anthony.delbee@gmail.com>
2025-03-26 16:39:14 +0100

    add error Page with error message

--- Views/templates/error.php
+++ Views/templates/error.php
@@ -3,15 +3,11 @@
  * Template pour afficher une page d'erreur.
  */
 
-//Variable HereDoc
-
-$previousUrl = Service::getPreviousUrl();
-
 $errorContent = <<<HTML
     <div class="error">
         <h2>Erreur</h2>
         <p>$errorMessage</p>
-        <a href="$previousUrl">Retour à la page précédente</a>
+        <a href="index.php">Retour à la page d'accueil</a>
     </div>
  HTML;
 

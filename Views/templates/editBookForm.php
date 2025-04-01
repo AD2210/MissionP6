5a38871fb7741ec7cd9b4223fce10ff361b2ccc1
Anthony Delbée <anthony.delbee@gmail.com>
2025-04-01 15:42:53 +0200

    clean all layout

--- Views/templates/editBookForm.php
+++ Views/templates/editBookForm.php
@@ -10,39 +10,41 @@ $availableClass = $available ? '' : 'selected'; //permet de selectionner la vale
 
 $dynamicLink = <<<HTML
     <section class="editBookSection">
-        <div>
-            <span><a href="index.php?action=privatePage">← retour</a></span>
-            <h1>Modifier les informations</h1>
-        </div>
-        <div class="editBookContainer">
-            <div class="editPictureContainer">
-                <p>Photo</p>
-                <img src="$picture" alt="photo du livre : $title">
-                <a href="#">Modifier la photo</a>
+        <div class="editBookMainContainer">
+            <div>
+                <span><a href="index.php?action=privatePage">← retour</a></span>
+                <h1>Modifier les informations</h1>
             </div>
-            <form class="editBookForm" action="index.php?action=updateBook&id=$id" method="POST">
-                <input type="hidden" name="id" value="$id">
-                <div class="formField">
-                    <label for="title">Titre</label>
-                    <input type="text" name="title" id="title" value="$title">
+            <div class="editBookContainer">
+                <div class="editPictureContainer">
+                    <p>Photo</p>
+                    <img src="$picture" alt="photo du livre : $title">
+                    <a href="#">Modifier la photo</a>
                 </div>
-                <div class="formField">
-                    <label for="author">Auteur</label>
-                    <input type="text" name="author" id="author" value="$author">
-                </div>
-                <div class="formField">
-                    <label for="comment">Commentaire</label>
-                    <textarea name="comment" id="comment" cols="" rows="">$comment</textarea>
-                </div>
-                <div class="formField">
-                    <label for="availability">Disponibilité</label>
-                    <select id="availability" name="availability">
-                        <option value="1">disponible</option>
-                        <option value="0" $availableClass>non dispo.</option>
-                    </select>
-                </div>
-                <button class ="button" type="submit">Valider</button>
-            </form>
+                <form class="editBookForm" action="index.php?action=updateBook&id=$id" method="POST">
+                    <input type="hidden" name="id" value="$id">
+                    <div class="formField">
+                        <label for="title">Titre</label>
+                        <input type="text" name="title" id="title" value="$title">
+                    </div>
+                    <div class="formField">
+                        <label for="author">Auteur</label>
+                        <input type="text" name="author" id="author" value="$author">
+                    </div>
+                    <div class="formField">
+                        <label for="comment">Commentaire</label>
+                        <textarea name="comment" id="comment" cols="" rows="20">$comment</textarea>
+                    </div>
+                    <div class="formField">
+                        <label for="availability">Disponibilité</label>
+                        <select id="availability" name="availability">
+                            <option value="1">disponible</option>
+                            <option value="0" $availableClass>non dispo.</option>
+                        </select>
+                    </div>
+                    <button class ="button" type="submit">Valider</button>
+                </form>
+            </div>
         </div>
     </section>
     HTML;
